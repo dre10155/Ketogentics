a72290b5c10a4bdc0dbc4ade6f8f7e177c525bee
dre10155 <andreas.mendes94@gmail.com>
2020-11-22 19:48:48 -0500

    error page & live test changes

---
+++ error.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Keto Smart</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
+</head>
+
+<body>
+    <h1>Error 404</h1>
+    <p>Error occurred completing your transaction.Please make sure payment details are entered correctly</p>
+    <a href="checkout.php">Please try again</a>
+
+
+</body>
+</html>
